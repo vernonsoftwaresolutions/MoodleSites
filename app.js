233ab74d7374218bc0d667a68c3b3e3ddb0613e1
andrewlarsen <andrew.larsen@concordusa.com>
2017-11-25 16:13:37 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict'
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const compression = require('compression')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const app = express()
+
+app.use(compression())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', (req, res) => {
+  const user = {
+    id: ++userIdCounter,
+    name: req.body.name
+  }
+  users.push(user)
+  res.status(201).json(user)
+})
+
+
+const getUser = (userId) => users.find(u => u.id === parseInt(userId))
+
+// Ephemeral in-memory data store
+const users = [{
+  id: 1,
+  name: 'Joe'
+}, {
+  id: 2,
+  name: 'Jane'
+}]
+let userIdCounter = users.length
+
+// The aws-serverless-express library creates a server and listens on a Unix
+// Domain Socket for you, so you can remove the usual call to app.listen.
+// app.listen(3000)
+
+// Export your express server so you can import it in the lambda function.
+module.exports = app
