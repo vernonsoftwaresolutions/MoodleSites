b3fd53bb7ca9da36d0f0b4e759328f79d06fe81f
andrewlarsen <andrew.larsen@concordusa.com>
2017-11-25 17:23:31 -0600

    fix hosted zone name

--- scripts/configure.js
+++ scripts/configure.js
@@ -42,7 +42,7 @@ const hostedzonename = args['hostedzonename']
 const stage = args['stage']
 const region = args.region
 const availableRegions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-west-2', 'eu-central-1', 'ap-northeast-1', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2']
-const stackname = arg['stackname']
+const stackname = args['stackname']
 
 //console.log(hostedzonename)
 
