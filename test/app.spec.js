6b6634873d4a841a5bae53190ae7ee313759b13a
andrewlarsen <andrew.larsen@concordusa.com>
2017-12-07 23:08:54 -0600

    add unit test with test coverage

---
+++ test/app.spec.js
@@ -0,0 +1,24 @@
+const app = require('../app')
+const request = require('supertest')
+const assert = require('chai').assert
+
+describe('GET /users', function() {
+    it('respond with json', function() {
+      return request(app)
+        .get('/users')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .then(response => {
+            assert(response.body.length, 2)
+        })
+    });
+});
+
+describe('POST /users', function() {
+    it('respond with json', function() {
+      return request(app)
+        .post('/users')
+        .set('Accept', 'application/json')
+        .expect(201)
+    });
+});
